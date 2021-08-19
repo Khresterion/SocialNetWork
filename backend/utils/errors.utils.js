ec939aed56c4c9d40d307550991faab60b79e924
Khresterion <axios.ludis@gmail.com>
2021-08-19 18:58:00 +0200

    feat: back cors end

--- backend/utils/errors.utils.js
+++ backend/utils/errors.utils.js
@@ -7,7 +7,7 @@ module.exports.signUpErrors = (err) => {
   if (err.message.includes("email")) errors.email = "Email incorrect";
 
   if (err.message.includes("password"))
-    errors.password = "Le mot de passe doit faire 6 caractères minium";
+    errors.password = "Le mot de passe doit faire 6 caractères minimum";
 
   if (err.code === 11000 && Object.keys(err.keyValue)[0].includes("pseudo"))
     errors.pseudo = "Ce pseudo est déjà pris";
@@ -25,6 +25,18 @@ module.exports.signInErrors = (err) => {
 
   if (err.message.includes("password"))
     errors.password = "Le mot de passe ne correspond pas";
+
+  return errors;
+};
+
+module.exports.uploadErrors = (err) => {
+  let errors = { format: "", maxSize: "" };
+
+  if (err.message.includes("invalid file"))
+    errors.format = "Format incompatabile";
+
+  if (err.message.includes("max size"))
+    errors.maxSize = "Le fichier dépasse 500ko";
 
   return errors;
 };
